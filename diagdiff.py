8fe05d43af7949d4ee07ad4815fe9cf5bc99f8e4
khushii-hub <72705845+khushii-hub@users.noreply.github.com>
2024-04-23 00:50:16 +0530

    Create diagdiff.py

---
+++ diagdiff.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    ltr = 0
+    rtl =0
+    for i in range(n):
+        ltr += arr[i][i]
+        rtl += arr[i][n - i - 1]
+        
+
+    diff = ltr - rtl
+    return abs(diff)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
